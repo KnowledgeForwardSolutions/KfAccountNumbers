743b683661e1d56a84996228a13de8acd78b81e3
Michael L. Harges <michael.harges@gmail.com>
2026-06-06 17:21:33 -0700

    Add GbPatientNumber record with full XML documentation
    Introduced GbPatientNumber in KfAccountNumbers.Governmental.Europe as a strongly-typed business object for UK patient identifiers (NHS, CHI, H&C, test). Includes detailed XML docs covering structure, validation, formatting, and type determination. Inherits from GbPatientNumberBase.

---
+++ src/KfAccountNumbers/Governmental/Europe/GbPatientNumber.cs
@@ -0,0 +1,245 @@
+namespace KfAccountNumbers.Governmental.Europe;
+
+/// <summary>
+///   Strongly typed business object that can represent any of the UK public
+///   health service patient identifiers, the NHS number used by the National
+///   Health Service (NHS) of England, Wales and the Isle of Man, the CHI number
+///   used by the Scottish Community Health Index (CHI) and the H&amp;C number
+///   used by Northern Ireland Health and Care.
+/// </summary>
+/// <remarks>
+///   <para>
+///      A GB Patient Number consists of 10 digits, with a structure that varies
+///      slightly, depending on the type of number. NHS numbers and H&amp;C
+///      numbers are structured as NNNNNNNNNC,
+///      where:
+///      <list type="bullet">
+///         <item>
+///            <term>NNNNNNNNN</term>
+///            <description>
+///               A unique nine-digit number assigned by the issuing health
+///               service.
+///            </description>
+///         </item>
+///         <item>
+///            <term>C</term>
+///            <description>
+///               A modulus 11 check digit calculated from the preceding nine
+///               digits.
+///            </description>
+///         </item>
+///      </list>
+///      CHI numbers are structured as DDMMYYNNGC, where:
+///      <list type="bullet">
+///         <item>
+///            <term>DDMMYY</term>
+///            <description>
+///               The patient date of birth encoded in DDMMYY format.
+///            </description>
+///         </item>
+///         <item>
+///            <term>NNG</term>
+///            <description>
+///               Three digits used to differentiate between two persons born on
+///               the same day. The third digit (G) also indicates the person's
+///               gender, where odd numbers = male and even numbers = female.
+///            </description>
+///         </item>
+///         <item>
+///            <term>C</term>
+///            <description>
+///               A modulus 11 check digit calculated from the preceding nine
+///               digits.
+///            </description>
+///         </item>
+///      </list>
+///      Scottish CHI numbers are the only UK patient number that encodes
+///      patient data in the number.
+///   </para>
+///   <para>
+///      All three types (NHS, H&amp;C and CHI) can be displayed as a string of
+///      10 digits or formatted for readability by including separator
+///      characters. NHS numbers and H&amp;C numbers are typically formatted as
+///      three groups of digits in a '3 3 4' pattern (e.g. "123 456 7890"). CHI
+///      numbers are typically formatted as two groups of digits, six digits for
+///      the date of birth and the four remaining digits. The optional separator
+///      characters can be any character that is not an ASCII digit ('0' - '9'),
+///      but NHS and H&amp;C numbers must use the same character in both
+///      separator character locations. The typical separator character is a
+///      space (' ').
+///   </para>
+///   <para>
+///      Each of the public health services (NHS, Scottish CHI and Northern
+///      Ireland H&amp;C) are allocated separate blocks of 10-digit numbers
+///      so it is possible to determine what service issued the number by
+///      comparing the number to a list of valid ranges for each service. For
+///      NHS, the valid ranges are 400 000 000 to 499 999 999 and 600 000 000 to
+///      799 999 999 (excluding the trailing check digit). For H&amp;C, the
+///      valid ranges are 320 000 000 to 399 999 999 (excluding the trailing
+///      check digit). For CHI, the valid range is 010 000 000 to 311 299 999
+///      (excluding the trailing check digit). The two leading digits of the
+///      valid range for CHI numbers correspond to the digits allowed by a valid
+///      date in DDMMYY format (i.e. 01-31). An additional range of numbers from
+///      900 000 000 to 999 999 999 are reserved for test purposes and are not
+///      issued to the public.
+///   </para>
+///   <para>
+///      GbPatientNumber includes an <see cref="IdentifierType"/> property which
+///      indicates the exact type of identifier, based on the ranges listed
+///      above. GbPatientNumber also includes methods to convert to the specific
+///      identifier type (<see cref="ToGbNhsNumber"/>,
+///      <see cref="ToGbChiNumber"/> and see cref="ToGbHcNumber"/>).
+///   </para>
+///   <para>
+///      When creating a new <see cref="GbPatientNumber"/>, the following
+///      validation rules are applied:
+///      <list type="bullet">
+///         <item>
+///            <description>
+///               The value may not be null, empty or all whitespace characters.
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               The value must be either 10 characters (without separators) or
+///               11 characters (a CHI number with one separator) or 12
+///               characters (NHS and H&amp;C numbers with 2 separators) in
+///               length.
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               All characters (except the optional separator characters) must
+///               be ASCII digits ('0'-'9').
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               The trailing (right-most) digit must be a valid Modulus 11
+///               check digit.
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               If the value is 11 characters long, character positions 6
+///               (zero-based) must not be an ASCII digit ('0' - '9').
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               If the value is 12 characters long, character positions 3 and
+///               7 (zero-based) must not be ASCII digits ('0' - '9'). The same
+///               character must be used in each separator position.
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               The first nine digits must fall in one of the following
+///               ranges: 010 000 000 to 311 299 999, 320 000 000 to 399 999 999,
+///               400 000 000 to 499 999 999, 600 000 000 to 799 999 999, or
+///               900 000 000 to 999 999 999.
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               The length of the input value must be appropriate for the
+///               identifier type, as determined by the range that the value
+///               falls into. NHS, H&amp;C and test numbers may have lengths of
+///               10 or 12 characters (two separators for length 12) while CHI
+///               numbers may have lengths of 10 or 11 characters (a single
+///               separator for length 11).
+///            </description>
+///         </item>
+///         <item>
+///            <description>
+///               For CHI number (as determined by the range that the value
+///               falls into), the first six digits must represent a valid date
+///               of birth in the format DDMMYY. When determining the validity
+///               of the date of birth, the default century cutoff of 50 is used,
+///               which results in year 00-49 = 2000-2049 and 50-99 = 1950-1999.
+///               The means that year 00 is considered a leap year which allows
+///               the date of birth 290200.
+///            </description>
+///         </item>
+///      </list>
+///   </para>
+///   <para>
+///      The Modulus 11 check digit algorithm used by the National Health
+///      Service and other issuing authorities generate a check value of 10
+///      which can not be encoded as a single decimal digit. NHS, H&amp;C and
+///      CHI avoid this issue by not issuing any number that would result in a
+///      check value of 10. This means that approximately 9.09% of all possible
+///      values are never issued.
+///   </para>
+///   <para>
+///      Example values:
+///      <list type="bullet">
+///         <item>
+///            <term>3112999991</term>
+///            <description>
+///               CHI number without formatting. Date of birth December 31,
+///               1999, gender = male.
+///            </description>
+///         </item>
+///         <item>
+///            <term>311299 9991</term>
+///            <description>
+///               The same, but formatted for readability.
+///            </description>
+///         </item>
+///         <item>
+///            <term>3200000007</term>
+///            <description>
+///               H&amp;C number without formatting.
+///            </description>
+///         </item>
+///         <item>
+///            <term>320 000 0007</term>
+///            <description>
+///               The same, but formatted for readability.
+///            </description>
+///         </item>
+///         <item>
+///            <term>4000000004</term>
+///            <description>
+///               NHS number without formatting.
+///            </description>
+///         </item>
+///         <item>
+///            <term>799 999 9997</term>
+///            <description>
+///               The same, but formatted for readability.
+///            </description>
+///         </item>
+///         <item>
+///            <term>9000000009</term>
+///            <description>
+///               Test number without formatting.
+///            </description>
+///         </item>
+///         <item>
+///            <term>900 000 0009</term>
+///            <description>
+///               The same, but formatted for readability.
+///            </description>
+///         </item>
+///      </list>
+///   </para>
+///   <para>
+///      See https://en.wikipedia.org/wiki/NHS_number,
+///      https://www.datadictionary.nhs.uk/attributes/nhs_number.html
+///      https://en.wikipedia.org/wiki/National_Health_Service_Central_Register#Community_Health_Index,
+///      https://www.datadictionary.nhs.uk/attributes/community_health_index_number.html?hl=chi%2Cnumber,
+///      https://www.datadictionary.nhs.uk/attributes/health_and_care_number.html?hl=number,
+///      and https://webarchive.nationalarchives.gov.uk/ukgwa/20231221081503/https://digital.nhs.uk/about-nhs-digital/contact-us/freedom-of-information/freedom-of-information-disclosure-log/december-2022/nic-690159-k8h4z
+///      for more info.
+///   </para>
+///   <para>
+///      Also see <see cref="GbChiNumber"/>, <see cref="GbHcNumber"/> and
+///      <see cref="GbNhsNumber"/> for associated patient identifier business
+///      objects.
+///   </para>
+/// </remarks>
+public record GbPatientNumber : GbPatientNumberBase
+{
+}
